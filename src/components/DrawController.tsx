e0a82efaa940fb3e0125fa51d38787c98eef602a
Rafael Mohlin <raffe_mohlin@hotmail.com>
2021-12-11 03:25:19 +0100

    Add poc for selected/selecting features from other components

--- src/components/DrawController.tsx
+++ src/components/DrawController.tsx
@@ -1,11 +1,37 @@
-import React, { useContext } from "react";
-import MapContext from "./MapContext";
+import React from "react";
+import { useMapContext } from "./MapContext";
 
 interface DrawControllerProps {
     shouldDraw: () => void;
 }
 
 export default function DrawController({ shouldDraw }: DrawControllerProps) {
-    const mapContext = useContext(MapContext);
-    return <button onClick={shouldDraw}>Draw</button>;
+    const { mapState, setSelectedDrawFeatures } = useMapContext();
+    return (
+        <>
+            <button onClick={shouldDraw}>Draw</button>
+            {mapState &&
+                Array.from(mapState.drawFeatures)
+                    .sort(([ida], [idb]) => ida.localeCompare(idb))
+                    .map(([id, feature]) => (
+                        <label key={id} className="border border-black">
+                            <input
+                                type="checkbox"
+                                checked={mapState.selectedDrawFeatures.has(id)}
+                                onChange={(e) => {
+                                    const newSelected = new Set(
+                                        mapState.selectedDrawFeatures
+                                    );
+                                    if (newSelected.has(id))
+                                        newSelected.delete(id);
+                                    else newSelected.add(id);
+                                    setSelectedDrawFeatures(newSelected);
+                                }}
+                            ></input>
+                            {feature.geometry.type}{" "}
+                            {feature.id?.toString().substring(0, 8)}
+                        </label>
+                    ))}
+        </>
+    );
 }
